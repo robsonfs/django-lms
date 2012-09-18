ef0bbd440acfaf792132254ba7c39fd2d5140e28
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-09-18 13:05:49 -0500

    Dropped unused template tag

--- libs/templatetags/courses.py
+++ libs/templatetags/courses.py
@@ -12,31 +12,6 @@ def course_menu_active(context, url):
     return ""
 
 @register.tag()
-def iffaculty(parser, token):
-    """ Check to see if the currently logged in user is faculty
-
-    """
-    nodelist = parser.parse(('endiffaculty',))
-    parser.delete_first_token()
-    return FacultyCheckNode(nodelist)
-
-
-class FacultyCheckNode(template.Node):
-    def __init__(self, nodelist):
-        self.nodelist = nodelist
-    def render(self, context):
-        user = resolve_variable('user', context)
-        if not user.is_authenticated:
-            return ''
-        try:
-            group = Group.objects.get(name='Faculty')
-        except Group.DoesNotExist:
-            return ''
-        if group in user.groups.all():
-            return self.nodelist.render(context)
-        return ''
-
-@register.tag()
 def ifcoursefaculty(parser, token):
     """
     Check to see if the currently logged in user is faculty for this course
