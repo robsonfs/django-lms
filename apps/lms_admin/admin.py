6961760581f0f16e1564ee9e068c773ad93d7603
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-10-08 13:15:23 -0500

    Added filtering to groups in admin

--- apps/lms_admin/admin.py
+++ apps/lms_admin/admin.py
@@ -1,4 +1,18 @@
+from django.contrib import admin
 from django.conf import settings
 
-if settings.NONREL:
-    from nonreladmin import *
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+class BetterUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        # Get form from original UserAdmin.
+        form = super(BetterUserAdmin, self).get_form(request, obj, **kwargs)
+        groups = form.base_fields['groups']
+        groups.widget = FilteredSelectMultiple("Groups", False)
+        return form
+
+
+admin.site.unregister(User)
+admin.site.register(User, BetterUserAdmin)
