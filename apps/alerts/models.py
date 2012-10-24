d8f5e46da649744276ff7d072a22bf8720f8d8fe
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-10-24 12:24:23 -0500

    Dropped the old fields

--- apps/alerts/models.py
+++ apps/alerts/models.py
@@ -16,8 +16,6 @@ class Alert(TimeStampedModel):
     # This is char because there are a wide variety that can send
     sent_by = models.CharField(max_length = 200)
     sent_to = models.ForeignKey(User)
-    created = models.DateTimeField(auto_now_add = True)
-    modified = models.DateTimeField(auto_now = True)
     title = models.CharField(max_length = 200)
     details = tinymce_models.HTMLField(blank = True)
     level = models.CharField(max_length = 200, choices = ALERT_LEVELS, blank = True)
