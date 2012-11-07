3f4c7a3cb6ecf1dbd036488e88d0200bb10473eb
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-11-07 10:54:41 -0600

    Added some factories

---
+++ apps/courses/factories.py
@@ -0,0 +1,23 @@
+import factory
+from datetime import datetime
+
+from models import Course, Semester
+
+class SemesterFactory(factory.Factory):
+    FACTORY_FOR = Semester
+
+    name = 'Fall'
+    year = unicode(datetime.now().year)
+    start = datetime.now().date() - timedelta(days=30)
+    end = datetime.now().date() + timedelta(days=30)
+    
+class CourseFactory(factory.Factory):
+    FACTORY_FOR = Course
+
+    title = 'Test Title'
+    section = '001'
+    number = '101'
+    description = 'Test Course<br>It is a test.'
+    semester = factory.LazyAttribute(lambda a: SemesterFactory())
+    campus = 'main'
+    location = '543'
