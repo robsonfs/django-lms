8979adb30c1fb17aa6d49f83e363a9790b49261f
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-11-29 12:14:44 -0600

    Added main app to test script

--- runtests.sh
+++ runtests.sh
@@ -3,7 +3,7 @@ export SITE_ID="1"
 export SECRET_KEY="10293802948abcabaacbacbacbabefeacbef"
 if [ $# -ne 1 ]
 then
-    python manage.py test springboard courses profiles alerts
+    python manage.py test springboard courses profiles alerts lms_main
 else
     python manage.py test $1
 fi
